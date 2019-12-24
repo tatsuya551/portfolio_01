4af5121065efd3606aca4aa6d8a45f7cc229b0cf
tatsuya551 <seki0515ya@gmail.com>
2019-12-24 15:35:58 +0900

    誤字修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: [~/.ssh/leothefb.pem]
+                  keys: ['~/.ssh/leothefb.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
