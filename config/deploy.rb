0329602fdfe7f6e57d94ecfe1f8179f085d232de
tatsuya551 <seki0515ya@gmail.com>
2019-12-25 15:51:53 +0900

    correction

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,13 +25,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
+
 
 
 set :linked_files, %w{ config/master.key }
