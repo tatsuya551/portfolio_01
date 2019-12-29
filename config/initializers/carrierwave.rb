02083a9f5e6b49ddec605ee0d80f972553e2d33b
tatsuya551 <56828853+tatsuya551@users.noreply.github.com>
2019-12-29 16:35:02 +0900

    user_mypage (#8)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,14 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
+# 開発環境ではローカルに保存
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+# 本番はS3に保存
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
@@ -14,4 +19,5 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'portfolio_01'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/portfolio_01'
+  end
 end
