43ebf9f682483ecb4f744fdf4691c7a85632280f
tatsuya551 <56828853+tatsuya551@users.noreply.github.com>
2019-12-29 19:10:10 +0900

    book_information_page (#9)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
   resources :signup, only: [:new, :create]
-  resources :books, only: [:new, :create]
+  resources :books, only: [:new, :create, :show]
   resources :users, only: [:index]
 end
