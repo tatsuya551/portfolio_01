62641b5e1d24ddbc5105a5596c4d1235e6c87bab
tatsuya551 <seki0515ya@gmail.com>
2019-12-30 16:25:52 +0900

    book_edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :set_books, only: [:show, :edit]
+  
   def new
     @book = Book.new
   end
@@ -12,11 +14,32 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if Book.update(book_params)
+      redirect_to users_path
+    else
+      render 'show'
+    end
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :author, :publisher, :reason, :date, :image).merge(user_id: current_user.id)
+    params.require(:book).permit(
+      :title,
+      :author,
+      :publisher,
+      :reason,
+      :date,
+      :image
+    ).merge(user_id: current_user.id)
+  end
+
+  def set_books
+    @book = Book.find(params[:id])
   end
 end
