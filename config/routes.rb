fa32d8d89bd8e338e58667a7dbfaa1a3a38db46c
tatsuya551 <56828853+tatsuya551@users.noreply.github.com>
2020-01-02 15:41:33 +0900

    login_logout_front_back (#18)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :signup, only: [:new, :create]
+  resources :signup, only: [:new, :create] do
+    collection do
+      get 'logout'
+    end
+  end
   resources :users, only: [:index]
   resources :books, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :impressions, only: [:new, :create]
