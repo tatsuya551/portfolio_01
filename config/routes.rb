02d765beaaba712885dbc08b22f3d8d7272aa43f
tatsuya551 <56828853+tatsuya551@users.noreply.github.com>
2020-01-07 20:15:23 +0900

    user_category_front_back (#28)

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update] do
     member do
       get 'category'
+      get 'novel'
+      get 'management'
+      get 'economy'
+      get 'finance'
+      get 'it'
+      get 'motivation'
+      get 'talent'
+      get 'etc'
     end
   end
   resources :books, only: [:new, :create, :show, :edit, :update, :destroy] do
