6bcfe3e39e4c31348841a6a5899e793aab521280
tatsuya551 <seki0515ya@gmail.com>
2020-01-13 14:33:50 +0900

    carrierwave_setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,7 +17,7 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'portfolio_01'
+  config.fog_directory  = 'portfolio551'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/portfolio_01'
   end
 end
