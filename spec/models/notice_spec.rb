40ebb57c3abc3ee39f6e86c4a33c4c9d7c6f5072
tatsuya551 <seki0515ya@gmail.com>
2020-01-28 15:00:40 +0900

    notice_model_test_code

--- spec/models/notice_spec.rb
+++ spec/models/notice_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe Notice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Notice, type: :model do
+  describe '#create' do
+
+    #############################
+    # 保存NG確認                 #
+    #############################
+    context 'can not save' do
+      it "[content] nil" do
+        notice = build(:notice, content: nil)
+        notice.valid?
+        expect(notice.errors.messages[:content]).to include("を入力してください")
+      end
+
+      it "[date] nil" do
+        notice = build(:notice, date: nil)
+        notice.valid?
+        expect(notice.errors.messages[:date]).to include("を入力してください")
+      end
+
+      it "[status] nil" do
+        notice = build(:notice, status: nil)
+        notice.valid?
+        expect(notice.errors.messages[:status]).to include("を入力してください")
+      end
+    end
+
+    #############################
+    # 保存OK確認                 #
+    #############################
+    context 'can save' do
+      it "[notice] all ok" do
+        notice = build(:notice)
+        notice.valid?
+        expect(notice).to be_valid
+      end
+
+      it "[impression_id] nil" do
+        notice = build(:notice, impression_id: nil)
+        notice.valid?
+        expect(notice).to be_valid
+      end
+    end
+  end
 end
