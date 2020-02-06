cdd9f07b2e8cc6c49e3e274912e9e8fd52766371
tatsuya551 <seki0515ya@gmail.com>
2020-02-06 19:49:59 +0900

    google_certification

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root "home#index"
   resources :signup, only: [:new, :create] do
