f27b5da86ae48bc44757abad24ee51abb4ac0c6c
tatsuya551 <seki0515ya@gmail.com>
2020-08-31 16:10:05 +0900

    signup_refactoring (#88)

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -6,20 +6,15 @@ class SignupController < ApplicationController
   end
 
   def create
-    if params[:sns_auth] == 'true'
-      pass = Devise.friendly_token
-      params[:user][:password] = pass
-      params[:user][:password_confirmation] = pass
-    end
+    # if params[:sns_auth] == 'true'
+    #   pass = Devise.friendly_token
+    #   params[:user][:password] = pass
+    #   params[:user][:password_confirmation] = pass
+    # end
     params[:user][:birth_day] = @birth_date.to_s
     @user = User.new(user_params)
-    if @user.save(user_params)
-      session[:nickname] = user_params[:nickname]
-      session[:email] = user_params[:email]
-      session[:password] = user_params[:password]
-      session[:birth_day] = user_params[:birth_day]
-      session[:id] = @user.id
-      sign_in User.find(session[:id]) unless user_signed_in?
+    if @user.save
+      sign_in User.find(@user.id) unless user_signed_in?
       redirect_to user_path(current_user.id)
     else
       render 'new'
